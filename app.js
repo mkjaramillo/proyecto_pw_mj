554d0b7bf161dd8572ad3dd023d9db4daf196dcd
mkjaramillo <mkjaramillo@uce.edu.ec>
2022-06-22 19:51:54 -0500

    primer clase de vue.js

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("objetos y librerias")
+console.log(Vue)
+
+const app=Vue.createApp({
+    //vue utiliza representacion declarativa para evaluar/ejecutar
+    template: `
+    <h1> hola mundo desde vue.js</h1>
+    <p>Expresion javaScript</p>
+    <p>{{1+1}}</p>
+    <h2> Arreglo</h2>
+    <p>{{[1,2,3,4,5]}}</p>
+    <h2>Objeto</h2>
+    <p>{{ {nombre:"Milena",apellido:"Jaramillo"} }}</p>
+    <h2>Funcion</h2>
+    <p>{{true ?'activo' : 'inactivo' }}</p>
+    `
+})
+app.mount('#myApp')
